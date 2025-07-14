fb6602ca8e2937d70aeba3c42dd699174430cf51
Cayuga007 <jayson.tram13@gmail.com>
2025-07-13 22:19:46 -0700

    feat: add ball roll

--- GolfBall.lua
+++ GolfBall.lua
@@ -1,14 +1,34 @@
 local GolfBall = {}
 GolfBall.__index = GolfBall
 
+local VELOCITY_DECREASE = 0.98 -- Decrease velocity over time to simulate friction
+
 function GolfBall.new(x, y, radius)
     local self = setmetatable({}, GolfBall)
 
     self.x = x
     self.y = y
-    self.radius = radius or 10
+    self.radius = radius
+
+    self.is_moving = false
+    self.velocity_x = 0
+    self.velocity_y = 0
 
     return self
+end
+
+function GolfBall:roll()
+    self.x = self.x + self.velocity_x
+    self.y = self.y + self.velocity_y
+    self.velocity_x = self.velocity_x * VELOCITY_DECREASE
+    self.velocity_y = self.velocity_y * VELOCITY_DECREASE
+
+    if math.abs(self.velocity_x) < 0.1 and math.abs(self.velocity_y) < 0.1 then
+        self.is_moving = false -- Stop moving if velocity is very low
+        self.velocity_x = 0
+        self.velocity_y = 0
+        print("Golf ball stopped moving")
+    end
 end
 
 function GolfBall:display()
--- main.lua
+++ main.lua
@@ -2,15 +2,20 @@ package.path = package.path .. ";./?.lua"
 local Player = require("Player")
 local GolfBall = require("GolfBall")
 
+local FORCE_SCALE = 20 -- Scale down the force applied to the golf ball
+
 function love.load()
     player = Player.new()
     golf_ball = GolfBall.new(400, 300, 10)
 end
 
 function love.update(dt)
-    if love.mouse.isDown(1) then
+    if love.mouse.isDown(1) and not golf_ball.is_moving then
         local mouse_x, mouse_y = love.mouse.getPosition()
         player:aim(mouse_x, mouse_y, golf_ball.x, golf_ball.y)
+    end
+    if golf_ball.is_moving then
+        golf_ball:roll()
     end
 end
 
@@ -23,6 +28,14 @@ end
 
 function love.mousereleased(x, y, button)
     if player.is_aiming and button == 1 then
-        player:shoot()
+        -- player:shoot() -- Send data to server
+        player.is_aiming = false
+
+        -- Get end position of ball from server
+        local force = player.magnitude / FORCE_SCALE -- Placeholder
+        local angle = player.angle -- Placeholder
+        golf_ball.velocity_x = -force * math.cos(angle)
+        golf_ball.velocity_y = -force * math.sin(angle)
+        golf_ball.is_moving = true
     end
 end
