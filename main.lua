4a0ec905c59272ea3318ed8e1137478b1a56baaf
Emily <eha80@sfu.ca>
2025-07-14 17:40:57 -0700

    finished ui of main menu

--- main.lua
+++ main.lua
@@ -2,15 +2,23 @@ package.path = package.path .. ";./?.lua"
 
 local menu = require("scenes/MainMenu")
 
+local hostButton
+local joinButton
+local quitButton
+
 function love.load()
-    love.graphics.setBackgroundColor(1, 1, 1) -- set white
+
+    -- images
+    hostButton = love.graphics.newImage("assets/img/hostButton.png")
+    joinButton = love.graphics.newImage("assets/img/joinButton.png")
+    quitButton = love.graphics.newImage("assets/img/quitButton.png")
 
 end
 
 
 function love.draw()
     menu.title()
-    menu.hostButton()
-    menu.joinButton()
-    menu.quitButton()
+    menu.hostButton(hostButton)
+    menu.joinButton(joinButton)
+    menu.quitButton(quitButton)
 end
--- scenes/MainMenu.lua
+++ scenes/MainMenu.lua
@@ -7,44 +7,30 @@ local fontButton = love.graphics.newFont("assets/dogicapixelbold.ttf", 30)
 
 function menu.title()
     -- color of text
-    love.graphics.setColor(0, 0, 0, 1)
 
     -- font size
     love.graphics.setFont(fontTitle)
 
     -- text
-    love.graphics.printf("MINIATURE GOLF", 0, height / 2 - 200, width, "center")
+    love.graphics.printf("MINIATURE", 0, height / 2 - 200, width, "center")
+        love.graphics.printf("GÖLF!", 0, height / 2 - 125, width, "center")
+
 end
 
-function menu.hostButton()
-    -- font
-    love.graphics.setFont(fontButton)
-
-    --  text
-    love.graphics.setColor(0, 0, 0, 1) -- black
-    love.graphics.printf("HOST A GAME", 0, height / 2, width, "center")
+-- host button
+function menu.hostButton(img)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(img, width/2, height/2, 0, 1, 1, 225)
 end
 
-function menu.joinButton()
-    -- font
-    love.graphics.setFont(fontButton)
-
-    -- dimensions
-    -- x, y, width, height
-    love.graphics.rectangle("line", width/3 +10, height / 2 + 75, 250, 25)
-
-    --  text
-    love.graphics.setColor(0, 0, 0, 1) -- black
-    love.graphics.printf("JOIN GAME", 0, height / 2 + 75, width, "center")
+function menu.joinButton(img)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(img, width/2, height/2+ 75, 0 , 1, 1, 225)
 end
 
-function menu.quitButton()
-    -- font
-    love.graphics.setFont(fontButton)
-
-    --  text
-    love.graphics.setColor(0, 0, 0, 1) -- black
-    love.graphics.printf("QUIT", 0, height / 2 + 150, width, "center")
+function menu.quitButton(img)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(img, width/2, height/2 + 150, 0, 1, 1, 225)
 end
 
 return menu;
