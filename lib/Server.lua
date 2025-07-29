6f2122637649fcd8fbcce9c74206fda9f1b9947f
camblsoup <cam.mclagan@gmail.com>
2025-07-28 20:33:22 -0700

    added methods to server.lua

--- lib/Server.lua
+++ lib/Server.lua
@@ -21,55 +21,22 @@ function Server.listen()
                 socket = client,
                 id = #clients
             }
-            table.insert(clients, clientObj)
+            table.insert(Server.clients, clientObj)
             print("New client connected! ID:", clientObj.id)
 
             -- Send welcome message
             client:send("WELCOME:Client " .. clientObj.id .. "\n")
         end
 
-        for i = #clients, 1, -1 do
-            local clientObj = clients[i]
-            local data, err = clientObj.socket:receive()
-
-            if data then
-                print("Received from client", clientObj.id, ":", data)
-
-                clientObj.socket:send("ECHO: " .. data)
-            elseif err == "closed" then
-                print("Client", clientObj.id, "disconnected")
-                clientObj.socket:close()
-                table.remove(clients, i)
-            end
+        if (#Server.clients >= 4) then
+            break
         end
     end
 end
 
---[[
-local Server = socket.bind("*", 12345)
-Server:settimeout(0)
-
-local clients = {}
-
-print("Server started on port 12345")
-
-while true do
-    local client = Server:accept()
-    if client then
-        client:settimeout(0)
-        local clientObj = {
-            socket = client,
-            id = #clients
-        }
-        table.insert(clients, clientObj)
-        print("New client connected! ID:", clientObj.id)
-
-        -- Send welcome message
-        client:send("WELCOME:Client " .. clientObj.id .. "\n")
-    end
-
-    for i = #clients, 1, -1 do
-        local clientObj = clients[i]
+function Server.play()
+    for i = #Server.clients, 1, -1 do
+        local clientObj = Server.clients[i]
         local data, err = clientObj.socket:receive()
 
         if data then
@@ -79,8 +46,7 @@ while true do
         elseif err == "closed" then
             print("Client", clientObj.id, "disconnected")
             clientObj.socket:close()
-            table.remove(clients, i)
+            table.remove(Server.clients, i)
         end
     end
 end
---]]
